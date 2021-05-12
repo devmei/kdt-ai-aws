f019905e899b85d8267eb2e029f79acd67ccc714
devmei <devmei.n@gmail.com>
2021-05-12 06:26:43 +0000

    AWS를 활용한 인공지능 모델 배포 VI - Add Deep Learning model handler

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import itertools
+import torch
 from utils import clean_text
 
 
@@ -62,16 +63,34 @@ class DLModelHandler(ModelHandler):
         self.initialize()
 
     def initialize(self, ):
-        pass
+        # De-serializing model and loading vectorizer
+        from transformers import AutoTokenizer, AutoModelForSequenceClassification
+        self.model_name_or_path = 'sackoh/bert-base-multilingual-cased-nsmc'
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name_or_path)
+        self.model = AutoModelForSequenceClassification.from_pretrained(self.model_name_or_path)
+        self.model.to('cpu')
 
-    def preprocess(self, data):
-        return data
+    def preprocess(self, text):
+        # preprocess raw text
+        model_input = self._clean_text(text)
+        # vectorizing cleaned text
+        model_input = self.tokenizer(text, return_tensors='pt', padding=True)
+        return model_input
 
-    def inference(self, data):
-        return data
+    def inference(self, model_input):
+        with torch.no_grad():
+            model_output = self.model(**model_input)[0].cpu()
+            model_output = 1.0 / (1.0 + torch.exp(-model_output))
+            model_output = model_output.numpy().astype('float')
+        return model_output
 
-    def postprocess(self, data):
-        return data
+    def postprocess(self, model_output):
+        predicted_probabilities = model_output.max(axis=1)
+        predicted_ids = model_output.argmax(axis=1)
+        predicted_labels = [self.id2label[id_] for id_ in predicted_ids]
+        return predicted_labels, predicted_probabilities
 
     def handle(self, data):
-        return data
+        model_input = self.preprocess(data)
+        model_output = self.inference(model_input)
+        return self.postprocess(model_output)
